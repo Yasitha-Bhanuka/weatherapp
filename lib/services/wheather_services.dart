e846873d25d1f9d390e20098067de2f48f037c59
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2024-12-24 12:55:22 +0530

    Add weather data fetching functionality to WheatherService

--- lib/services/wheather_services.dart
+++ lib/services/wheather_services.dart
@@ -1,5 +1,21 @@
+import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class WheatherService {
   final String apiKey = dotenv.env['WEATHER_API_KEY'] ?? '';
+
+  Future<Map<String, dynamic>> getWeather(String cityName) async {
+    // Fetch weather data from API
+    final response = await http.get(Uri.parse(
+        'api.openweathermap.org/data/2.5/weather?q=$cityName,uk&APPID=$apiKey'));
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, then parse the JSON.
+      return jsonDecode(response.body);
+    } else {
+      // If the server returns an error response, then throw an exception.
+      throw Exception('Failed to load weather data');
+    }
+  }
 }
